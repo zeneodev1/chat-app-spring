26371fd38d157eaf7221579ef892b38bcd9c2560
Mohamed <zeneodev1@gmail.com>
2020-08-06 19:26:25 +0100

    first commit

---
+++ src/main/java/com/zeneo/chatappspring/ChatAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.zeneo.chatappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zeneo/chatappspring/ChatAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeneo.chatappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
